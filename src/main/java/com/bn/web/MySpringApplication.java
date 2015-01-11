c12dd099741aab17bb0682938f1e06b5f23c2aa8
Senthil <ssggkk@gmail.com>
2015-01-10 19:36:14 -0500

    Push will fail..

--- src/main/java/com/bn/web/MySpringApplication.java
+++ src/main/java/com/bn/web/MySpringApplication.java
@@ -10,7 +10,7 @@ import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
-/*
+/* Try pushing..
  * This class initiates Spring Boot Framework..
  */
 @Configuration
